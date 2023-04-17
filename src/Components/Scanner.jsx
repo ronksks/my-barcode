01f2eece22fa1a05b9359ea0814175e8c3b20ce7
Ron <32454051+ronksks@users.noreply.github.com>
2023-04-17 12:09:19 +0300

    wideth 80% .form + qrbox wcreen width

--- src/Components/Scanner.jsx
+++ src/Components/Scanner.jsx
@@ -1,25 +1,61 @@
-import { Html5QrcodeScanner } from "html5-qrcode";
+import { Html5QrcodeScanner, Html5QrcodeScanType } from "html5-qrcode";
+import React, { useEffect, useState } from "react";
+// import "./BarcodeScanner.css";
 
-function onScanSuccess(decodedText, decodedResult) {
-  // handle the scanned code as you like, for example:
-  console.log(`Code matched = ${decodedText}`, decodedResult);
-}
+function ScannerComponent({ childToParentScanner, cameraToggle }) {
+  useEffect(() => {
+    if (Html5QrcodeScanner) {
+      let html5QrcodeScanner = new Html5QrcodeScanner(
+        "reader",
 
-function onScanFailure(error) {
-  // handle scan failure, usually better to ignore and keep scanning.
-  // for example:
-  console.warn(`Code scan error = ${error}`);
-}
+        {
+          fps: 10,
+          disableFlip: false,
+          focusMode: "continuous",
+          advanced: [{ zoom: 2.0 }],
+          rememberLastUsedCamera: true,
+          experimentalFeatures: {
+            useBarCodeDetectorIfSupported: true,
+          },
+          willReadFrequently: true,
+          supportedScanTypes: [
+            // Html5QrcodeScanType.SCAN_TYPE_FILE,
+            Html5QrcodeScanType.SCAN_TYPE_CAMERA,
+          ],
+          qrbox: {
+            width: window.screen.width < 600 ? 200 : 300,
+            height: window.screen.width < 600 ? 100 : 100,
+          },
+        },
+        /* verbose= */ false
+      );
 
-function Scanner() {
-  let html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader",
-    { fps: 10, qrbox: { width: 250, height: 250 } },
-    /* verbose= */ false
-  );
-  html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+      function onScanSuccess(data) {
+        childToParentScanner(data);
+
+        // Stop scanning
+        html5QrcodeScanner
+          .clear()
+          .then((_) => {
+            // the UI should be cleared here
+          })
+          .catch((error) => {
+            console.log("Closing Camera Error");
+            // Could not stop scanning for reasons specified in `error`.
+            // This conditions should ideally not happen.
+          });
+      }
+
+      function onScanFailure(error) {
+        cameraToggle(html5QrcodeScanner);
+        // handle scan failure, usually better to ignore and keep scanning
+        console.warn(`QR error = ${error}`);
+      }
+
+      html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+    }
+  }, [Html5QrcodeScanner]);
 
   return <div id="reader"></div>;
 }
-
-export default Scanner;
+export default ScannerComponent;
