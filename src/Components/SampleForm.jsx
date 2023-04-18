19817be5b56b971335d1dca5627b056eab5ecac2
Ron <32454051+ronksks@users.noreply.github.com>
2023-04-18 12:46:04 +0300

    alert values sampleBags from sampleForm

--- src/Components/SampleForm.jsx
+++ src/Components/SampleForm.jsx
@@ -46,7 +46,7 @@ const SampleForm = () => {
         onSubmit={(values) => {
           console.log("submited");
           console.log(values.sampleBags);
-          alert(values.sampleBags[values.sampleBags.length]);
+          alert(values.sampleBags);
         }}
       >
         {({ values, touched, isSubmitting }) => (
