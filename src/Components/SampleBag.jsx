174225fbf138c7bcca33d7b98f7444d484610e32
Ron <32454051+ronksks@users.noreply.github.com>
2023-04-18 08:49:23 +0300

    back to working versoion

--- src/Components/SampleBag.jsx
+++ src/Components/SampleBag.jsx
@@ -8,9 +8,80 @@ const SampleBag = ({ bagNumber, index }) => {
   const [scannedData, setScannedData] = useState("");
   const [showScanner, setShowScanner] = useState(false);
 
-  function handleScannedData(data) {
-    setScannedData(data);
-    setShowScanner(false); // hide the scanner component
+  // function handleScannedData(data) {
+  //   setScannedData(data);
+  //   setShowScanner(false); // hide the scanner component
+  // }
+
+  function handleScanButtonClick() {
+    // This method will trigger user permissions
+    Html5Qrcode.getCameras()
+      .then((devices) => {
+        /**
+         * devices would be an array of objects of type:
+         * { id: "id", label: "label" }
+         */
+        if (devices && devices.length) {
+          // .. use this to start scanning.
+          const html5QrCode = new Html5Qrcode("reader");
+          const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+            /* handle success */
+            setScannedData(decodedText);
+            console.log(scannedData);
+            html5QrCode
+              .stop()
+              .then((ignore) => {
+                // QR Code scanning is stopped.
+              })
+              .catch((err) => {
+                // Stop failed, handle it.
+              });
+          };
+          const config = {
+            fps: 100,
+            qrbox: {
+              width: window.screen.width < 600 ? 200 : 300,
+              height: window.screen.width < 600 ? 100 : 100,
+            },
+            aspectRatio: 1,
+          };
+
+          // ************  Back Camera hardcoded
+          try {
+            html5QrCode.start(
+              { facingMode: { exact: "environment" } },
+              config,
+              qrCodeSuccessCallback
+            );
+            // wait 2 seconds to guarantee the camera has already started to apply the focus mode and zoom...
+            setTimeout(function () {
+              html5QrCode.applyVideoConstraints({
+                focusMode: "continuous",
+                advanced: [{ zoom: 2.0 }],
+              });
+            }, 2000);
+          } catch (error) {
+            console.log("Unable to start scanning.", error);
+          }
+
+          // // ************  Back Camera
+          // html5QrCode.start(
+          //   { deviceId: { exact: cameraId } },
+          //   config,
+          //   qrCodeSuccessCallback
+          // );
+
+          // *** front camera
+          // html5QrCode.start(
+          //   { facingMode: "user" },
+          //   config,
+          //   qrCodeSuccessCallback
+          // );
+        }
+      })
+      .catch((err) => {
+        // handle err
+      });
   }
   //
   return (
@@ -22,12 +93,12 @@ const SampleBag = ({ bagNumber, index }) => {
         <button
           type="button"
           className="btn btn-secondary"
-          onClick={handleScannedData}
+          onClick={handleScanButtonClick}
         >
           Scan
         </button>
-        {/* <div id="reader"></div> */}
-        {showScanner && <ScannerComponent scannedData={handleScannedData} />}
+        <div id="reader"></div>
+        {/* {showScanner && <ScannerComponent scannedData={handleScannedData} />} */}
 
         <Field
           type="text"
