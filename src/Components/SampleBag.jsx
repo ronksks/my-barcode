3fb803457dac1bb7cba5af0247a4d8c9818cf62b
Ron <32454051+ronksks@users.noreply.github.com>
2023-04-18 09:32:56 +0300

    new version

--- src/Components/SampleBag.jsx
+++ src/Components/SampleBag.jsx
@@ -1,89 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Field, ErrorMessage } from "formik";
-// import { Html5QrcodeScanner } from "html5-qrcode";
 import { Html5Qrcode } from "html5-qrcode";
-import ScannerComponent from "./ScannerComponent";
 
 const SampleBag = ({ bagNumber, index }) => {
   const [scannedData, setScannedData] = useState("");
   const [showScanner, setShowScanner] = useState(false);
-
-  // function handleScannedData(data) {
-  //   setScannedData(data);
-  //   setShowScanner(false); // hide the scanner component
-  // }
+  const qrCodeRef = useRef(null); // reference to QR code element
+  const readerId = `reader-${index}`;
 
   function handleScanButtonClick() {
-    // This method will trigger user permissions
-    Html5Qrcode.getCameras()
-      .then((devices) => {
-        /**
-         * devices would be an array of objects of type:
-         * { id: "id", label: "label" }
-         */
-        if (devices && devices.length) {
-          // .. use this to start scanning.
-          const html5QrCode = new Html5Qrcode("reader");
-          const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-            /* handle success */
-            setScannedData(decodedText);
-            console.log(scannedData);
-            html5QrCode
-              .stop()
-              .then((ignore) => {
-                // QR Code scanning is stopped.
-              })
-              .catch((err) => {
-                // Stop failed, handle it.
-              });
-          };
-          const config = {
-            fps: 100,
-            qrbox: {
-              width: window.screen.width < 600 ? 200 : 300,
-              height: window.screen.width < 600 ? 100 : 100,
-            },
-            aspectRatio: 1,
-          };
-
-          // ************  Back Camera hardcoded
-          try {
-            html5QrCode.start(
-              { facingMode: { exact: "environment" } },
-              config,
-              qrCodeSuccessCallback
-            );
-            // wait 2 seconds to guarantee the camera has already started to apply the focus mode and zoom...
-            setTimeout(function () {
-              html5QrCode.applyVideoConstraints({
-                focusMode: "continuous",
-                advanced: [{ zoom: 2.0 }],
-              });
-            }, 2000);
-          } catch (error) {
-            console.log("Unable to start scanning.", error);
-          }
-
-          // // ************  Back Camera
-          // html5QrCode.start(
-          //   { deviceId: { exact: cameraId } },
-          //   config,
-          //   qrCodeSuccessCallback
-          // );
-
-          // *** front camera
-          // html5QrCode.start(
-          //   { facingMode: "user" },
-          //   config,
-          //   qrCodeSuccessCallback
-          // );
+    // show the scanner component and initialize the scanner
+    setShowScanner(true);
+    const html5QrCode = new Html5Qrcode(readerId);
+    html5QrCode
+      .start(
+        {
+          facingMode: "environment",
+          aspectRatio: 1,
+          fps: 10,
+          qrbox: { width: 200, height: 200 },
+        },
+        (decodedText, decodedResult) => {
+          // handle success
+          setScannedData(decodedText);
+          setShowScanner(false);
+          html5QrCode.stop();
         }
-      })
-      .catch((err) => {
-        // handle err
+      )
+      .catch((error) => {
+        console.log("Unable to start scanning.", error);
       });
   }
-  //
+
   return (
     <div className="sample-bag">
       <h4>Sample Bag {bagNumber}</h4>
@@ -97,8 +45,7 @@ const SampleBag = ({ bagNumber, index }) => {
         >
           Scan
         </button>
-        <div id="reader"></div>
-        {/* {showScanner && <ScannerComponent scannedData={handleScannedData} />} */}
+        <div id={readerId} ref={qrCodeRef} style={{ display: "none" }}></div>
 
         <Field
           type="text"
@@ -131,6 +78,143 @@ const SampleBag = ({ bagNumber, index }) => {
 };
 
 export default SampleBag;
+
+//************ working ************ */
+// import React, { useState } from "react";
+// import { Field, ErrorMessage } from "formik";
+// // import { Html5QrcodeScanner } from "html5-qrcode";
+// import { Html5Qrcode } from "html5-qrcode";
+// import ScannerComponent from "./ScannerComponent";
+
+// const SampleBag = ({ bagNumber, index }) => {
+//   const [scannedData, setScannedData] = useState("");
+//   const [showScanner, setShowScanner] = useState(false);
+//   // generate a unique id for this SampleBag component
+//   const readerId = `reader-${index}`;
+
+//   // function handleScannedData(data) {
+//   //   setScannedData(data);
+//   //   setShowScanner(false); // hide the scanner component
+//   // }
+
+//   function handleScanButtonClick() {
+//     // This method will trigger user permissions
+//     Html5Qrcode.getCameras()
+//       .then((devices) => {
+//         /**
+//          * devices would be an array of objects of type:
+//          * { id: "id", label: "label" }
+//          */
+//         if (devices && devices.length) {
+//           // .. use this to start scanning.
+//           const html5QrCode = new Html5Qrcode("reader");
+//           const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+//             /* handle success */
+//             setScannedData(decodedText);
+//             console.log(scannedData);
+//             html5QrCode
+//               .stop()
+//               .then((ignore) => {
+//                 // QR Code scanning is stopped.
+//               })
+//               .catch((err) => {
+//                 // Stop failed, handle it.
+//               });
+//           };
+//           const config = {
+//             fps: 100,
+//             qrbox: {
+//               width: window.screen.width < 600 ? 200 : 300,
+//               height: window.screen.width < 600 ? 100 : 100,
+//             },
+//             aspectRatio: 1,
+//           };
+
+//           // ************  Back Camera hardcoded
+//           try {
+//             html5QrCode.start(
+//               { facingMode: { exact: "environment" } },
+//               config,
+//               qrCodeSuccessCallback
+//             );
+//             // wait 2 seconds to guarantee the camera has already started to apply the focus mode and zoom...
+//             setTimeout(function () {
+//               html5QrCode.applyVideoConstraints({
+//                 focusMode: "continuous",
+//                 advanced: [{ zoom: 2.0 }],
+//               });
+//             }, 2000);
+//           } catch (error) {
+//             console.log("Unable to start scanning.", error);
+//           }
+
+//           // // ************  Back Camera
+//           // html5QrCode.start(
+//           //   { deviceId: { exact: cameraId } },
+//           //   config,
+//           //   qrCodeSuccessCallback
+//           // );
+
+//           // *** front camera
+//           // html5QrCode.start(
+//           //   { facingMode: "user" },
+//           //   config,
+//           //   qrCodeSuccessCallback
+//           // );
+//         }
+//       })
+//       .catch((err) => {
+//         // handle err
+//       });
+//   }
+//   //
+//   return (
+//     <div className="sample-bag">
+//       <h4>Sample Bag {bagNumber}</h4>
+
+//       <div className="form-group">
+//         <label htmlFor={`sampleBags.${index}.barcode`}>Barcode:</label>
+//         <button
+//           type="button"
+//           className="btn btn-secondary"
+//           onClick={handleScanButtonClick}
+//         >
+//           Scan
+//         </button>
+//         <div id="reader"></div>
+//         {/* {showScanner && <ScannerComponent scannedData={handleScannedData} />} */}
+
+//         <Field
+//           type="text"
+//           id={`sampleBags.${index}.barcode`}
+//           name={`sampleBags.${index}.barcode`}
+//           value={scannedData}
+//         />
+//         <ErrorMessage
+//           className="error"
+//           component="div"
+//           name={`sampleBags.${index}.barcode`}
+//         />
+//       </div>
+
+//       <div className="form-group">
+//         <label htmlFor={`sampleBags.${index}.weight`}>Weight (grams):</label>
+//         <Field
+//           type="text"
+//           id={`sampleBags.${index}.weight`}
+//           name={`sampleBags.${index}.weight`}
+//         />
+//         <ErrorMessage
+//           className="error"
+//           component="div"
+//           name={`sampleBags.${index}.weight`}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SampleBag;
 
 // function handleScanButtonClick() {
 //   // This method will trigger user permissions
